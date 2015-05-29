42661713968dbd961bc110d49a1189d207e2ae84
Anton Sudak <asudak@geeksforless.net>
2015-05-29 19:18:14 +0300

    Created stub for web services Webservices based on JAX-RS located in org.javatraining.ws package. They depend on FlexJSON library that provide handy JSON serialization and deserialization. Also, unit test was created. At this moment unit test are useless (there only two test cases).

---
+++ cms/cms-core/src/main/java/org/javatraining/ws/ServiceConfig.java
@@ -0,0 +1,12 @@
+package org.javatraining.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by asudak on 5/29/15.
+ */
+@ApplicationPath("/ui")
+public class ServiceConfig extends Application {
+
+}
