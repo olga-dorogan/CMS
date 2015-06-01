011befca02e5400f18906f7718a5dadcc44d226d
Anton Sudak <anton.sudak@gmail.com>
2015-06-02 02:27:32 +0300

    Separate web service config for each namespace

---
+++ cms/cms-core/src/main/java/org/javatraining/ws/config/TeacherServiceConfig.java
@@ -0,0 +1,12 @@
+package org.javatraining.ws.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by asudak on 5/29/15.
+ */
+@ApplicationPath("/resources/teacher")
+public class TeacherServiceConfig extends Application {
+
+}
