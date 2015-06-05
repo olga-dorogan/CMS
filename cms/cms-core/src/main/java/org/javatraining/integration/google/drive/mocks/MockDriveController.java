ddfec469e9305ffe91503cd2a4e69abd8fd2c86d
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-06-05 13:38:32 +0300

    добавлены rest-cервис, сервлет и  html-страница для проверки работы с Google Drive

---
+++ cms/cms-core/src/main/java/org/javatraining/integration/google/drive/mocks/MockDriveController.java
@@ -0,0 +1,53 @@
+package org.javatraining.integration.google.drive.mocks;
+
+import org.javatraining.integration.google.drive.DriveService;
+import org.javatraining.model.PersonVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by olga on 03.06.15.
+ */
+@WebServlet("drive")
+public class MockDriveController extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(MockDriveController.class);
+    private static final String PARAM_METHOD = "_method";
+    private static final String PARAM_EMAIL = "email";
+    @EJB
+    DriveService driveService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String param = req.getParameter(PARAM_METHOD);
+        if (param.equals("get")) {
+            driveService.getFilesList();
+            resp.getWriter().write("list was successfully returned; see log");
+            return;
+        } else if (param.equals("delete_files")) {
+            driveService.removeFiles();
+            resp.getWriter().write("all files were deleted");
+            return;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String param = req.getParameter(PARAM_EMAIL);
+        if (param != null) {
+            PersonVO personVO = new PersonVO();
+            personVO.setEmail(param);
+            driveService.setOwnerAccessToFiles(personVO);
+            resp.getWriter().write("owner is added successfully");
+            return;
+        }
+        resp.getWriter().write("error with passing parameter");
+    }
+}
