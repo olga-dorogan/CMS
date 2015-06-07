bf17da237daf675befb2ad8e337426a67a3f6553
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-06-07 15:37:56 +0300

    добавлен CourseService

---
+++ cms/cms-core/src/main/java/org/javatraining/service/impl/CourseServiceImpl.java
@@ -0,0 +1,97 @@
+package org.javatraining.service.impl;
+
+import org.javatraining.model.CourseVO;
+import org.javatraining.model.NewsVO;
+import org.javatraining.model.PersonRoleVO;
+import org.javatraining.model.PersonVO;
+import org.javatraining.service.CourseService;
+import org.javatraining.service.exception.UnsupportedOperationException;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created by olga on 07.06.15.
+ */
+public class CourseServiceImpl implements CourseService {
+    @Override
+    public CourseVO save(@NotNull @Valid CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CourseVO update(@NotNull @Valid CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(@NotNull CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<CourseVO> getAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CourseVO getById(@NotNull Long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CourseVO fillPersons(@NotNull @Valid CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CourseVO fillNews(@NotNull @Valid CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public CourseVO fillLessons(@NotNull @Valid CourseVO courseVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addPersonsToCourse(@NotNull CourseVO courseVO, @NotNull @Valid List<PersonVO> persons) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removePersonsFromCourse(@NotNull CourseVO courseVO, @NotNull @Valid List<PersonVO> persons) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<PersonVO> getAllPersonsFromCourseByRole(@NotNull CourseVO courseVO, @NotNull PersonRoleVO role) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public NewsVO addNewsToCourse(@NotNull CourseVO courseVO, @NotNull @Valid NewsVO newsVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removeNewsFromCourse(@NotNull CourseVO courseVO, @NotNull NewsVO newsVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public NewsVO updateNews(@NotNull @Valid NewsVO newsVO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public NewsVO getNewsById(@NotNull Long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<NewsVO> getAllNews() {
+        throw new UnsupportedOperationException();
+    }
+}
