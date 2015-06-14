ca05261157789acf7c507a59d297f4cdaf61a924
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-06-14 22:43:40 +0300

    merged profiles for jenkins

---
+++ cms/cms-core/src/test/java/EmbeddedArquillianTest.java
@@ -0,0 +1,23 @@
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by olga on 14.06.15.
+ */
+@RunWith(Arquillian.class)
+public class EmbeddedArquillianTest {
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class);
+    }
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
