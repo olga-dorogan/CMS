d6c29d0a6baabf24c8e6b12b79c12399124c2ac9
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-06-16 15:02:27 +0300

    phone field is added to database preloading script

--- cms/cms-core/src/main/resources/META-INF/sql/data-h2.sql
+++ cms/cms-core/src/main/resources/META-INF/sql/data-h2.sql
@@ -1,1 +1,1 @@
-INSERT INTO person(id, email, name, second_name, last_name, personRole) VALUES(1, 'dorogan.olga.test@gmail.com', 'test', 'test', 'test', 'TEACHER');
+INSERT INTO person(id, email, phone, name, second_name, last_name, personRole) VALUES(1, 'dorogan.olga.test@gmail.com', '123', 'test', 'test', 'test', 'TEACHER');
--- cms/cms-core/src/test/resources/META-INF/sql/test-person-service.sql
+++ cms/cms-core/src/test/resources/META-INF/sql/test-person-service.sql
@@ -1,4 +1,4 @@
-INSERT INTO person(id, email, name, second_name, last_name, personRole) VALUES(1, 'teacher@gmail.com', 'teacherName', 'teacherSecondName', 'teacherLastName', 'TEACHER');
-INSERT INTO person(id, email, name, second_name, last_name, personRole) VALUES(2, 'student@gmail.com', 'studentName', 'studentSecondName', 'studentLastName', 'STUDENT');
+INSERT INTO person(id, email, phone, name, second_name, last_name, personRole) VALUES(1, 'teacher@gmail.com', '123', 'teacherName', 'teacherSecondName', 'teacherLastName', 'TEACHER');
+INSERT INTO person(id, email, phone, name, second_name, last_name, personRole) VALUES(2, 'student@gmail.com', '123', 'studentName', 'studentSecondName', 'studentLastName', 'STUDENT');
 INSERT INTO courses(id, name, description, startdate, enddate) VALUES (1, 'courseName', 'courseDescription', '2014-01-10', '2015-07-31');
 INSERT INTO person_course(course_id, person_id) VALUES (1, 1);
