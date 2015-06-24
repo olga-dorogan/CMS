968557cfb9f93a321d6b9a6b753d5d7c92931ab4
Anton Sudak <asudak@geeksforless.net>
2015-06-24 18:33:27 +0300

    Refactored/created classes to work with lessons.

---
+++ cms/cms-core/src/main/java/org/javatraining/service/LessonService.java
@@ -0,0 +1,27 @@
+package org.javatraining.service;
+
+import org.javatraining.model.LessonVO;
+
+import javax.annotation.Nullable;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+/**
+ * Created by asudak on 6/24/15.
+ */
+public interface LessonService {
+    void save(@NotNull @Valid LessonVO lessonVO);
+    LessonVO update(@NotNull @Valid LessonVO lessonVO);
+    void remove(@NotNull LessonVO lessonVO);
+
+    @Valid
+    @Nullable
+    LessonVO getById(@NotNull Long id);
+
+    @Nullable
+    @Valid
+    Set<LessonVO> getByCourseId(@NotNull Long courseId);
+
+
+}
