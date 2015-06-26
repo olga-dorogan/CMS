baa48051b5aff7c1a1b39ca46cff4b70e82ae6b6
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-06-26 12:47:44 +0300

    dao exceptions are marked as application exceptions

--- cms/cms-core/src/main/java/org/javatraining/dao/exception/EntityIsAlreadyExistException.java
+++ cms/cms-core/src/main/java/org/javatraining/dao/exception/EntityIsAlreadyExistException.java
@@ -1,8 +1,11 @@
 package org.javatraining.dao.exception;
 
+import javax.ejb.ApplicationException;
+
 /**
  * Created by vika on 23.06.15.
  */
+@ApplicationException(rollback = true)
 public class EntityIsAlreadyExistException extends RuntimeException {
     public EntityIsAlreadyExistException() {
         super();
