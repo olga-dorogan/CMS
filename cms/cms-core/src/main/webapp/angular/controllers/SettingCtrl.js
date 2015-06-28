713f8387d09df81fbbf40ff8a4a599977c8d80e5
Sergey Bavykin <codedealerb@gmail.com>
2015-06-29 00:09:06 +0300

    configure person servise and setting controller in angular js

--- cms/cms-core/src/main/webapp/angular/controllers/SettingCtrl.js
+++ cms/cms-core/src/main/webapp/angular/controllers/SettingCtrl.js
@@ -2,9 +2,10 @@ function SettingCtrl($scope, $window, PersonService) {
     $scope.person = $scope.person || {};
     $scope.person.name = $window.localStorage['name'].split(" ")[0];
     $scope.person.surname = $window.localStorage['name'].split(" ")[1];
+    $scope.person.description = PersonService.getPersonDescription() || {};
 
     $scope.isValidUser = function () {
-        if(!this.isValidTextFields()){
+        if (!this.isValidTextFields()) {
             $scope.messages = 'Проверьте Ваши введённые ФИО. Поле не может быть пустым!';
             $scope.alertStatus = 'warning';
         }
@@ -22,7 +23,7 @@ function SettingCtrl($scope, $window, PersonService) {
         $scope.person.phoneNumber = $scope.person.phoneNumber.replace(/[^\d]/g, '');//замена символов на пустые, для ввода только цифр
     };
 
-    $scope.updatePerson = function(){
-
+    $scope.updatePerson = function () {
+        PersonService.updatePerson($scope.person);//FIXME finally refresh fields of person
     }
 }
--- cms/cms-core/src/main/webapp/angular/services/PersonService.js
+++ cms/cms-core/src/main/webapp/angular/services/PersonService.js
@@ -1,35 +1,46 @@
 function PersonService(Restangular) {
     var Person = Restangular.all("resources/person");
 
+    this.getPersonDescription = function(){
+        var PersonDescription = Person.get($window.localStorage['id']).one("description");
+
+        return PersonDescription.getList()[0];//Возвращение описание человека для личного кабинета
+    };
+
     this.createPerson = function (user) {
         var name = user.name.split(" ");
+        var PersonDescription = Person.one("description");
         return Person.post(
-            {
-                "email": user.email,
-                "id": null,
-                "lastName": name[1],
-                "name": name[0],
-                "personRole": null,
-                "secondName": null
-            }
-        );
-    };
-
-    //this.getPersonDescription()
-
-    this.updatePerson = function (user){
-        return Person.put(
-            {
+                {
+                    "email": user.email,
+                    "id": null,
+                    "lastName": name[1],
+                    "name": name[0],
+                    "personRole": null,
+                    "secondName": null
+                }
+            ) && PersonDescription.post({//FIXME maybe this could be removed
                 "id": $window.localStorage['id'],
-                "name" : user.name,
-                "surname" : user.surname,
-                "secondName" : user.seconName,
-                "experience" : user.experience,
-                "graduation" : user.graduation,
-                "phoneNumber" : user.phoneNumber
-            }
-        );
+                "experience": null,
+                "graduation": null,
+                "phoneNumber": null
+            });
     };
+
+    this.updatePerson = function (user) {
+        var PersonDescription = Person.get($window.localStorage['id']).one("description");
+        return Person.get($window.localStorage['id']).put(
+                {
+                    "name": user.name,
+                    "surname": user.surname,
+                    "secondName": user.seconName
+                }
+            ) && PersonDescription.put({
+                "experience": user.experience,
+                "graduation": user.graduation,
+                "phoneNumber": user.phoneNumber
+            });
+    };//FIXME проверка этого метода на правильность урлов
 
     this.getTeachers = function () {
         return Person.getList({'role': 'teacher'}).$object;
