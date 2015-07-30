97ac07f335666b2f7083c7cb01ca5723d88a0aa2
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-07-30 13:43:51 +0300

    UI: updated authorization with redirection to the requested page

--- cms/cms-core/src/main/webapp/angular/modules/person.js
+++ cms/cms-core/src/main/webapp/angular/modules/person.js
@@ -14,6 +14,9 @@ angular.module('myApp.person', ['ui.router'])
                         controller: 'PersonCtrl'
                     }
                 },
+                data: {
+                    authRequired: true
+                },
                 resolve: {
                     courseService: 'CourseService',
                     personService: 'PersonService',
